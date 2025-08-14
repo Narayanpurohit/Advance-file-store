dd4f3fba62dc6f1bec23f6820800c13fcc848157
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-08-14 21:28:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 # database.py
 from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME
+from config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
 db = mongo_client["filestore"]
