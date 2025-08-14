57faa900bb5cd737e147c8f7f9ee123b1b7a5402
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-08-14 18:58:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://hp108044:zWy9AuflXmsrAfSY@clus
 ADMINS = list(map(int, os.getenv("ADMINS", "5597521952").split(",")))
 
 # Verification system toggle
-VERIFICATION_MODE = os.getenv("VERIFICATION_MODE", "False").lower() == "false"
+VERIFICATION_MODE = os.getenv("VERIFICATION_MODE", "True").lower() == "false"
 
 # Premium settings
 PREMIUM_HOURS_VERIFICATION = int(os.getenv("PREMIUM_HOURS_VERIFICATION", "8"))
