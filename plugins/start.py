340f4a55aca78853dbd2d58cda970f57373f1782
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-08-15 15:44:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -66,4 +66,4 @@ async def start_handler(client, message):
         await message.reply_text("❌ Unknown file type.")
 
     # 8. Increment file send counter
-    increment_stat()
+    increment_stat(1)
