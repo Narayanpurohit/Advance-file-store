04bd5374c77477347131ebdecee82144a0d790d9
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-08-15 14:52:24 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
-from database import get_stats
+from database import get_stat
 from config import ADMINS
 
 @Client.on_message(filters.user(ADMINS) & filters.command("stats"))
 async def stats_command(client, message):
-    total_users, files_sent = get_stats()
+    total_users, files_sent = get_stat()
     await message.reply_text(f"📊 **Bot Stats**\n👥 Users: {total_users}\n📤 Files Sent: {files_sent}")
