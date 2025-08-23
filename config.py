3e6808d7e7a3469336d3e14ee2d55daa0646269d
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-08-23 21:35:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_NAME = os.getenv("DB_NAME", "filestore")
 ADMINS = list(map(int, os.getenv("ADMINS", "5597521952").split(",")))
 
 # Verification system toggle
-VERIFICATION_MODE = os.getenv("VERIFICATION_MODE", "True").lower() == "false"
+VERIFICATION_MODE = os.getenv("VERIFICATION_MODE", "True")
 
 # Premium settings
 PREMIUM_HOURS_VERIFICATION = int(os.getenv("PREMIUM_HOURS_VERIFICATION", "8"))
