37df969f2c18252c85ab9f5f38e974d75fc79a47
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-09-09 06:49:32 +0530

    Update status.py

--- og_plugins/status.py
+++ og_plugins/status.py
@@ -1,1 +1,7 @@
-#
+@Client.on_message(filters.command("status") & filters.private)
+async def status_handler(client, message):
+    user_id = message.from_user.id
+    user = users_col.find_one({"USER_ID": user_id}) or {}
+    bot_status = user.get("BOT_STATUS", "stopped")
+
+    await message.reply_text(f"ℹ️ Bot status: **{bot_status}**.")
