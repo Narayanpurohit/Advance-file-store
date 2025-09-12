2a07688160fa23fb551a8fb2d8b8247aac831611
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-09-12 06:48:32 +0530

    Update premium.py

--- og_plugins/premium.py
+++ og_plugins/premium.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from db_config import users_col
 
-@Client.on_message(filters.command("premium") & filters.private)
+@Client.on_message(filters.command("points") & filters.private)
 async def premium_handler(client, message):
     user_id = message.from_user.id
     user = users_col.find_one({"USER_ID": user_id}) or {}
