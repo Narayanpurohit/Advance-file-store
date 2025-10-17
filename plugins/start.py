36d14f93570dee72a556d723bc21da1b42612bc3
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-10-17 21:09:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,7 +20,7 @@ log = logging.getLogger(__name__)
 
 START_BUTTONS = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton("• 🤖 ᴄʀᴇᴀᴛᴇ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ •", url="https://t.me/File_store_clone_robot")],
+        [InlineKeyboardButton("• 🤖 ᴄʀᴇᴀᴛᴇ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ •", url="https://t.me/Zoro1001bot")],
         [
             InlineKeyboardButton("• 📚 ʜᴇʟᴘ •", callback_data="help"),
             InlineKeyboardButton("• ✖️ ᴄʟᴏsᴇ •", callback_data="close"),
