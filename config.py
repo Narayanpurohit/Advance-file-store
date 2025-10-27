acd3ce94e728a1189dd7f3065a3ec8a259f55868
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-10-27 12:46:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ CODE2_API_ID = int(os.getenv("CODE2_API_ID", "15191874"))
 CODE2_API_HASH = os.getenv("CODE2_API_HASH", "3037d39233c6fad9b80d83bb8a339a07")
 CODE2_MONGO_URI = os.getenv("CODE2_MONGO_URI", "mongodb+srv://hp108044:zWy9AuflXmsrAfSY@cluster0.zlecn7m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 CODE2_DB_NAME = os.getenv("CODE2_DB_NAME", "clone_maker")
+CODE2_LOG_CHANNEL=os.getenv("CODE2_LOG_CHANNEL", "-1002299128264")
 
 CODE2_ADMINS = list(map(int, os.getenv("CODE2_ADMINS", "5597521952").split(",")))
