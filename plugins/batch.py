61626a30f33f26553855b3a885c24dbef771d868
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-10-27 19:27:24 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -3,7 +3,7 @@ import random
 import string
 from pyrogram import Client, filters
 from database import save_batch
-from bot import ADMINS, PUBLIC_BOT
+from bot import PUBLIC_BOT, #ADMINS
 
 log = logging.getLogger(__name__)
 
