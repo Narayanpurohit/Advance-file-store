86903cbd6b0cfcf6b052f9e392e245806c785bd5
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-12-02 06:03:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
  
 
-CODE2_BOT_TOKEN = os.getenv("CODE2_BOT_TOKEN", "7630512584:AAHpwSS62a8_wdeSgrP7z96tk_ynZ72OeJI")
+CODE2_BOT_TOKEN = os.getenv("CODE2_BOT_TOKEN", "7630512584:AAFxtTVSemQ6bySeXG9vJD5AYtN0Dmpl2WI")
 CODE2_API_ID = int(os.getenv("CODE2_API_ID", "15191874"))
 CODE2_API_HASH = os.getenv("CODE2_API_HASH", "3037d39233c6fad9b80d83bb8a339a07")
 CODE2_MONGO_URI = os.getenv("CODE2_MONGO_URI", "mongodb+srv://hp108044:zWy9AuflXmsrAfSY@cluster0.zlecn7m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
