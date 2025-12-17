0bdcba419a68b0b23ed9fef07c70188e356d19dc
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-12-17 18:39:40 +0530

    Update file_handler.py

--- plugins/file_handler.py
+++ plugins/file_handler.py
@@ -130,7 +130,7 @@ async def generate_link(client, message):
             "file_type": file_type,
             "file_name": file_name,
             "file_size": file_size,
-            "caption": caption
+            "caption": caption,
             "slug2":slug2
         }
 
